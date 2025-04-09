bf62bdd031e1ced813c316005bc49dadb583bed2
Erisalene <145609923+Erisalene@users.noreply.github.com>
2025-04-08 20:45:12 -0500

    Add files via upload

---
+++ lab 4(commit1).py
@@ -0,0 +1,24 @@
+text = "Python — це мова програмування, яка дозволяє швидко розробляти проєкти будь-якої складності. Вона є простою у вивченні, потужною у застосуванні та має велику спільноту."
+
+# === Студент 1: Давиденко Федір ===
+# Операції:
+# - lower(): зниження регістру
+# - count(): підрахунок символів
+# - replace(): заміна слів
+
+
+lower_text = text.lower()
+count_u = lower_text.count("у")
+replaced_text = lower_text.replace("python", "Пайтон")
+words = text.split()
+position = text.find("розробляти")
+starts_with_python = text.startswith("Python")
+
+print("=== Студент 1: Давиденко Федір ===")
+print("Текст у нижньому регістрі:\n", lower_text)
+print("Кількість символів 'у':", count_u)
+print("Текст із заміною слова 'python':\n", replaced_text)
+print("=== Студент 2: Давиденко Федір ===")
+print("Список слів:", words)
+print("Позиція слова 'розробляти':", position)
+print("Чи починається текст зі слова 'Python'?:", starts_with_python)
